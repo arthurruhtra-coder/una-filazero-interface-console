7539bf8b14840b930b703629c5367f6e21d0b3a0
arthurruhtra-coder <arthurcarvalhooliveira1@gmail.com>
2026-04-10 20:32:27 -0300

    Add files via upload

---
+++ FilaZeroLogin/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        DesenharTela();
+        ExecutarLogin();
+    }
+
+    static void DesenharTela()
+    {
+        Console.Clear();
+        Console.WriteLine("=====================================");
+        Console.WriteLine("         Fila Zero - Login           ");
+        Console.WriteLine("=====================================");
+    }
+
+    static void ExecutarLogin()
+    {
+        Console.Write("---USUÁRIO---: ");
+        string username = Console.ReadLine() ?? "";
+
+        Console.Write("---SENHA---: ");
+        string password = Console.ReadLine() ?? "";
+
+        if (username == "Tuzinn420" && password == "hesoyam123")
+        {
+            Console.WriteLine("\nLogin bem-sucedido! Bem-vindo, Tuzinn420.");
+        }
+        else
+        {
+            Console.WriteLine("\nLogin falhou! Nome de usuário ou senha incorretos.");
+        }
+    }
+}
